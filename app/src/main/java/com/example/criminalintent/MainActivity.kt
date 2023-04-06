2aaa1c7a5850527976961d30eb5ef1be8dcbc115
kimms <kimms245@brandi.co.kr>
2023-04-06 21:14:12 +0900

    add CrimeListFragment

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.criminalintent
 
+import CrimeListFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // fragment 확인
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-
         if (currentFragment == null) {
-            val fragment = CrimeFragment()
+            // fragment instantiate
+            val fragment = CrimeListFragment.newInstances()
+            // manager에 add
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment)
                 .commit()
         }
