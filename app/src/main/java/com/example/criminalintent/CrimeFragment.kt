2c3a2bf7050b7ebcaaba62c9c630309d988713d1
kimms <kimms245@brandi.co.kr>
2023-04-06 16:43:06 +0900

    Fragment Crime 생성

---
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -0,0 +1,94 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+const val TAG: String = "CrimeFragment"
+
+class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) Called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        titleField = view.findViewById(R.id.crime_title)
+        dateButton = view.findViewById(R.id.crime_date)
+        solvedCheckBox = view.findViewById(R.id.crime_solved)
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        Log.d(TAG, "onCreateView")
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy")
+    }
+}
